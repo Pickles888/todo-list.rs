c85ad20f6ffbb47bcef184a0d8e39a21574384aa
Pickles888 <verysaltypickles@gmail.com>
2024-05-06 14:15:41 -0700

    added options and removed panic cases

--- src/main.rs
+++ src/main.rs
@@ -23,23 +23,27 @@ fn main() {
 
     let mut todo_items = parse_file();
     todo_items.sort_by(|a, b| a.id.cmp(&b.id)); 
-    /*vec![
-        TodoItem::new(1, "walk dog".to_string()),
-        TodoItem::new(3, "eat food".to_string()),
-        TodoItem::new(6, "BEANS".to_string()),
-    ];*/
+    
+    match {
+        match args.len() { 
+            1 => None,
+            _ => Some(args[1].as_str()),
+        }
+    } {
+        Some("list") => print_todo(&todo_items),
+        Some("add") => add_item(&mut todo_items, &args),
+        Some("rm") => remove_item(&mut todo_items, &args),
+        Some("complete") => complete_item(&mut todo_items, &args),
+        Some("help") => print_help(),
+        Some(&_) => {
+            eprintln!("Invalid Usage: '{}'", args[1]);
+            print_help();
+            std::process::exit(1);
+        },
+        None => print_todo(&todo_items),
+    };
 
-    //complete_item(&mut todo_items, &args);
-
-    print_todo(&todo_items);
-
-    write_file(&todo_items); 
-    /*match args[1] {
-        list => print_todo(&mut todo_items),
-        add => add_todo(),
-        remove => remove_todo(),
-        _ => print_help(),
-    }*/
+    write_file(&todo_items);
 }
 
 fn print_todo(todo_items: &Vec<TodoItem>) {
@@ -47,6 +51,12 @@ fn print_todo(todo_items: &Vec<TodoItem>) {
         .iter()
         .filter(|a| !a.completed)
         .for_each(|a| println!("{:<3}│ {}", a.id, a.name));
+}
+
+fn print_help() {
+    let nl = newline_os();
+
+    println!("USAGE: todo [COMMAND] [ID]{0}{0}COMMANDS:{0}add\tAdds an item{0}remove\tRemoves an item{0}list\tLists todo{0}help\tPrints this", nl);
 }
 
 fn write_file(todo_items: &Vec<TodoItem>) {
@@ -73,7 +83,12 @@ fn parse_file() -> Vec<TodoItem> {
     let mut todo_items: Vec<TodoItem> = vec![];
     let todo_file: String = fs::read_to_string("todo").expect("Could not read file: todo");
 
-    let todo_items_full = todo_file.strip_suffix(newline_os()).expect("Stripped suffix on none value.").split(newline_os());
+    if todo_file.is_empty() { return todo_items }
+
+    let todo_items_full = todo_file
+        .strip_suffix(newline_os())
+        .expect("Stripped suffix on none value.")
+        .split(newline_os());
 
     for i in todo_items_full {
         let split_items = i.split("^^").collect::<Vec<&str>>();
@@ -101,8 +116,7 @@ fn parse_file() -> Vec<TodoItem> {
 }
 
 fn complete_item(todo_items: &mut Vec<TodoItem>, args: &Vec<String>) {
-    //let id: u32 = args[2].trim().parse().unwrap();
-    let id: u32 = 1;
+    let id: u32 = args[2].trim().parse().unwrap();
 
     todo_items
         .iter_mut()
@@ -112,15 +126,29 @@ fn complete_item(todo_items: &mut Vec<TodoItem>, args: &Vec<String>) {
 
 fn add_item(todo_items: &mut Vec<TodoItem>, args: &Vec<String>) {
     let item_name = &args[2];
-    let max_item = todo_items.iter().max_by_key(|a| a.id);
 
     todo_items.push(
         TodoItem::new(
-            max_item.unwrap().id + 1,
+            {
+                match todo_items.is_empty() {
+                    true => 0,
+                    false => todo_items
+                        .iter()
+                        .max_by_key(|a| a.id)
+                        .unwrap()
+                        .id,
+                }
+            } + 1,
             item_name.to_string(),
             false
         )
     );
+}
+
+fn remove_item(todo_items: &mut Vec<TodoItem>, args: &Vec<String>) {
+    let id = args[2].trim().parse().unwrap();
+
+    todo_items.retain(|a| a.id != id);
 }
 
 fn newline_os() -> &'static str {
