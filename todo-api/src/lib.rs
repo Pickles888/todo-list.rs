31a8c0db83937f88e7e5ee14ef118c3f5eedaae4
Pickles888 <verysaltypickles@gmail.com>
2024-07-02 10:54:00 -0700

    added TodoListList with saving system

--- todo-api/src/lib.rs
+++ todo-api/src/lib.rs
@@ -1,6 +1,17 @@
 use std::{fs, io::{Error, ErrorKind}};
 use serde::{Deserialize, Serialize};
-use serde_json::Result;
+
+fn get_save_dir() -> Result<String, Error> {
+    Ok(format!(
+        "{}{}",
+        dirs::cache_dir()
+            .ok_or(ErrorKind::NotFound)?
+            .into_os_string()
+            .into_string()
+            .expect("Invalid Cache Directory Name"),
+        "/todo_list.rs/save.json"
+    ))
+}
 
 #[derive(Serialize, Deserialize)]
 pub enum TodoType {
@@ -43,7 +54,6 @@ impl TodoItem {
 pub struct TodoList {
     pub items: Vec<TodoItem>,
     pub name: String,
-    pub finished: bool,
 }
 
 impl TodoList {
@@ -51,11 +61,10 @@ impl TodoList {
         TodoList {
             items: vec![],
             name: name.to_string(),
-            finished: false,
         }
     }
 
-    pub fn finished(&self) -> bool {
+    pub fn is_finished(&self) -> bool {
         self.items.iter().all(|a| a.state == TodoState::Done)
     }
 
@@ -66,11 +75,53 @@ impl TodoList {
     pub fn remove(&mut self, index: usize) {
         self.items.remove(index);
     }
+}
 
-    pub fn save(&self) -> Result<(), Error> {
-        let contents = serde_json::to_string(&self)?;
-        fs::write(dirs::cache_dir().ok_or(ErrorKind::NotFound)?.into_os_string(), contents);
-        Ok(())
+struct TodoListList {
+    pub todolists: Vec<TodoList>,
+}
+
+impl TodoListList {
+    pub fn new() -> Self {
+        let mut todolistlist = TodoListList {
+            todolists: vec![],
+        };
+
+        todolistlist.add("Todo");
+        todolistlist
+    }
+
+    pub fn save(&self) {
+        let contents = serde_json::to_string(&self.todolists).unwrap();
+        let _ = fs::write(
+            get_save_dir().expect("Could Not Find Cache Directory."),
+            contents
+        );
+    }
+
+    pub fn load() -> Self {
+        match fs::metadata(get_save_dir().expect("Could not find save.json")) {
+            Ok(_) => println!("Save file found"),
+            Err(_) => {
+                println!("Save file not found, creating new");
+                let todolistlist = TodoListList::new();
+                todolistlist.save();
+            },
+        }
+
+        let todolists: Vec<TodoList> = serde_json::from_str(
+            &fs::read_to_string(get_save_dir().expect("Could Not Get Save Directory")).expect("Could not read save.json")
+        ).expect("Could not parse json from save.json");
+
+        TodoListList {todolists}
+    }
+
+    pub fn all_finished(&self) -> bool {
+        self.todolists.iter().all(|a| a.is_finished())
+    }
+
+    pub fn add(&mut self, name: &str) {
+        self.todolists.push(TodoList::new(name));
     }
 }
 
@@ -85,6 +136,6 @@ mod tests {
         test_list.add("other stuff");
         test_list.items[0].complete();
         test_list.items[1].complete();
-        assert!(test_list.finished())
+        assert!(test_list.is_finished())
     }
 }
